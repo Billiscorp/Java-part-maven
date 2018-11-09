6c76866b9d533b2ab9f066dc5fe2f1b7dbc30193
billiscorp <billiscorp@gmail.com>
2018-11-09 12:46:12 +0200

    Exercise all done

--- src/main/java/com/telesens/academy/lesson15/Exercise.java
+++ src/main/java/com/telesens/academy/lesson15/Exercise.java
@@ -1,45 +1,170 @@
 package com.telesens.academy.lesson15;
 
 import com.telesens.academy.automationpractice.model.EntityDress;
+import com.telesens.academy.lesson11.CompareDresses;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Exercise {
 
-        // TODO - конвертировать List в массив
+    public static void main(String[] args) {
+
+        EntityDress [] array = CompareDresses.getExpectedDresses();
+
+        System.out.println("конвертировать массив в List");
+        System.out.println("Array: ");
+        System.out.println(Arrays.toString(array));
+        List<EntityDress> list = convertArrayToList(array);
+        System.out.println("List: ");
+        System.out.println(list);
+
+
+        System.out.println("***********************************");
+        System.out.println("конвертировать List в массив");
+        EntityDress[] arrayFromList = convertListToArray(list);
+        System.out.println("Array from List:");
+        System.out.println(Arrays.toString(arrayFromList));
+
+        System.out.println("***********************************");
+        System.out.println("конвертировать массив в Map в качестве ключа взять поле model");
+        System.out.println("Array to map: ");
+        Map<String, EntityDress> map = convertArrayToMap(array);
+        System.out.println(map);
+
+
+
+        System.out.println("***********************************");
+        System.out.println("конвертировать отфильтровать List, оставив только позиции по заданному цвету");
+        System.out.println("Filter list by orange: ");
+        List<EntityDress> filteredList = filterByColor(list, "Orange");
+        System.out.println(filteredList);
+
+
+        System.out.println("***********************************");
+        System.out.println("конвертировать Map в List");
+        System.out.println("Map to List");
+        System.out.println("Map: ");
+        System.out.println(map);
+        List<EntityDress> entityDressList = convertMapToList(map);
+        System.out.println("List: ");
+        System.out.println(entityDressList);
+
+
+        System.out.println("***********************************");
+        System.out.println("конвертировать List в Массив (массив содержит только String - значения поля model)");
+        System.out.println("List to Array (String model)");
+        System.out.println("List: ");
+        System.out.println(list);
+        System.out.println("Array: ");
+        String[] entityDressesModel = convertListToModelArray (list);
+        System.out.println("Array entityDressesModel: ");
+        System.out.println(Arrays.toString(entityDressesModel));
+
+
+        System.out.println("***********************************");
+        System.out.println("проверить, равны ли два списка");
+        System.out.println("List1: ");
+        System.out.println(list);
+        System.out.println("List2: ");
+        System.out.println(entityDressList);
+
+        boolean EqualLists = checkEqualLists(list, entityDressList);
+
+        System.out.println("checkEqualLists = " + EqualLists);
+
+
+
+
+
+
+
+    }
+
+        // конвертировать List в массив
         public static EntityDress[] convertListToArray(List<EntityDress> list) {
-            return null;
+
+                  return list.toArray(new EntityDress[]{});
+                  //  return (EntityDress[]) list.toArray();
+        }
+         // #2
+        // конвертировать массив в List
+        public static List<EntityDress> convertArrayToList(EntityDress[] array){
+
+                      return Arrays.asList(array);
         }
 
-        // TODO - конвертировать массив в List
-        public static List<EntityDress> convertArrayToList(EntityDress[] array) {
-            return null;
-        }
-
-        // TODO - конвертировать массив в Map в качестве ключа взять поле model
+        // конвертировать массив в Map в качестве ключа взять поле model
         public static Map<String, EntityDress> convertArrayToMap(EntityDress[] array) {
-            return null;
-        }
+            Map<String, EntityDress> map = new HashMap<>();
 
-        // TODO - проверить, равны ли два списка
+            // 1 способ
+            for (int i = 0; i<array.length; i++) {
+                        map.put(array[i].getModel(), array[i]);
+                    }
+                    return map;
+                }
+
+
+              // 2 Способ
+           //        for (int i = 0; i<array.length; i++) {
+           //            String model = array[i].getModel();
+           //            EntityDress dress = array[i];
+           //            map.put(model, dress);
+           //        }
+           //        return map;
+
+
+
+                   /*// 3 способ
+                   for (EntityDress dress: array) {
+                       map.put(dress.getModel(), dress);
+                   }
+                   return map;
+               }*/
+
+
+
+    
+
+
+        // проверить, равны ли два списка
         public static boolean checkEqualLists(List<EntityDress> list1, List<EntityDress> list2) {
-            return false;
+
+            Collections.sort(list1);
+            Collections.sort(list2);
+            return list1.equals(list2);
         }
 
-        // TODO - конвертировать Map в List
+        // конвертировать Map в List
         public static List<EntityDress> convertMapToList(Map<String, EntityDress> map) {
-            return null;
+
+            List<EntityDress> list = new ArrayList<>(map.values());
+            return list;
         }
 
-        // TODO - конвертировать List в Массив (массив содержит только String - значения поля model)
+        // конвертировать List в Массив (массив содержит только String - значения поля model)
         public static String[] convertListToModelArray(List<EntityDress> list) {
-            return null;
+
+            String [] arrayModels = new String[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                arrayModels[i] = list.get(i).getModel();
+            }
+//            Iterator<E> iterator ?????
+            return arrayModels;
         }
 
-        // TODO - конвертировать отфильтровать List, оставив только позиции по заданному цвету
+        // конвертировать отфильтровать List, оставив только позиции по заданному цвету
         public static List<EntityDress> filterByColor(List<EntityDress> list, String color) {
-            return null;
+
+            List<EntityDress> filteredList = new ArrayList<>();
+            for (EntityDress dress : list){
+                if (dress.getColor().equals(color)){
+                    filteredList.add(dress);
+                }
+            }
+            return filteredList;
         }
+
+
 
 }
