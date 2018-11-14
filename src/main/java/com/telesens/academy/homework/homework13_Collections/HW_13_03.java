576aba24e1ae7d553c493e825e7b1370d458a781
billiscorp <billiscorp@gmail.com>
2018-11-14 15:20:14 +0200

    Homework12 Collections

--- src/main/java/com/telesens/academy/homework/homework13_Collections/HW_13_03.java
+++ src/main/java/com/telesens/academy/homework/homework13_Collections/HW_13_03.java
@@ -9,9 +9,21 @@ package com.telesens.academy.homework.homework13_Collections;
 */
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HW_13_03 {
 
     public static void main(String[] args) {
+
+        //        a) создать список целых чисел
+        System.out.println("создать список целых чисел");
+        List<Integer> integersList = new ArrayList<>();
+        System.out.println(integersList);
+        System.out.println("****************************************");
+        System.out.println();
+
+        // TODO
 
 
     }
